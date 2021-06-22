33928da7511c469d236269b0537bae093aa03e53
Heber Nickison Pietrafeza <heber.sistweb@gmail.com>
2021-06-22 10:23:58 -0300

    Add project files.

---
+++ XBIMToolkitEssentials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XBIMToolkitEssentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
