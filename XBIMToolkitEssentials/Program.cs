052a8ed0d28fe4f47884d9ea5b19a82e8354090d
Heber Nickison Pietrafeza <heber.sistweb@gmail.com>
2021-06-22 12:29:26 -0300

    Tutorial Spatial hierarchy from IFC
    Url: https://docs.xbim.net/examples/spatial-hierarchy.html

--- XBIMToolkitEssentials/Program.cs
+++ XBIMToolkitEssentials/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using XBIMToolkitEssentials.Services;
 
 namespace XBIMToolkitEssentials
 {
@@ -6,7 +7,7 @@ namespace XBIMToolkitEssentials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SpatialStructureExample.Show();
         }
     }
 }
---
+++ XBIMToolkitEssentials/Services/SpatialStructureExample.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.Common;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+
+namespace XBIMToolkitEssentials.Services
+{
+    class SpatialStructureExample
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Digite o nome do arquivo");
+            var nameFile = Console.ReadLine();
+            string file = Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"FileTests\", nameFile);
+            using (var model = IfcStore.Open(file))
+            {
+                var proj = model.Instances.FirstOrDefault<IIfcProject>();
+                PrintHierarchy(proj, 0);
+            }
+        }
+
+        private static void PrintHierarchy(IIfcObjectDefinition proj, int v)
+        {
+            Console.WriteLine(string.Format("{0}{1}[{2}]", GetIndent(0), proj.Name, proj.GetType().Name));
+            //apenas elementos espaciais podem conter elementos de construção
+            var spatialElement = proj as IIfcSpatialStructureElement;
+
+            if (spatialElement != null)
+            {
+                // usando IfcRelContainedInSpatialElement para obter os elementos contidos
+                var containedElements = spatialElement.ContainsElements.SelectMany(x => x.RelatedElements);
+                foreach (var element in containedElements)
+                {
+                    Console.WriteLine(string.Format("{0}    ->{1}[{2}]", GetIndent(0), element.Name, element.GetType().Name));
+                }
+            }
+
+            // usando IfcRelAggregares para obter decomposição espacial de elementos de estrutura espacial
+            var relatedObjects = proj.IsDecomposedBy.SelectMany(x => x.RelatedObjects);
+            foreach (var item in relatedObjects)
+            {
+                PrintHierarchy(item, v + 1);
+            }
+        }
+
+        private static string GetIndent(int v)
+        {
+            var indent = "";
+            for (int i = 0; i < v; i++)
+            {
+                indent = "  ";
+            }
+            return indent;
+        }
+    }
+}
