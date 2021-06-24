5b975f71e5a2bc71d561c10c6eabaf6d0934edde
Heber Nickison Pietrafeza <heber.sistweb@gmail.com>
2021-06-24 13:49:30 -0300

    Criação de projeto web para visualização de arquivos wexBIM testes

---
+++ TutorialEventfulBuildingWebUi/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace TutorialEventfulBuildingWebUi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public FileResult RetornoArquivoIFC(string file)
+        {
+            var path = Path.Combine(HostingEnvironment.ApplicationPhysicalPath,@"FileWexBIMTests\", file+".wexBIM");
+            var fileStream = System.IO.File.OpenRead(path);
+            
+            return File(fileStream, System.Net.Mime.MediaTypeNames.Application.Octet);
+        }
+    }
+}
