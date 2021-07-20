1b7df4326b3abb2e3ace4d62bf44f4e088a0bebc
Heber Nickison Pietrafeza <heber.sistweb@gmail.com>
2021-07-20 18:02:17 -0300

    Criação de projeto para converter ifc para wexbim

---
+++ ConvertIFCToWexbim/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConvertIFCToWexbim.Services;
+using System;
+
+namespace ConvertIFCToWexbim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ConvertIFC To Wexbim";
+            IFCToWexbim.Execute();
+        }
+    }
+}
---
+++ ConvertIFCToWexbim/Services/IFCToWexbim.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using Xbim.Ifc;
+using Xbim.ModelGeometry.Scene;
+
+namespace ConvertIFCToWexbim.Services
+{
+    class IFCToWexbim
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("Digite o nome do arquivo, ou nada para sair!");
+            var nameFile = Console.ReadLine();
+            if (string.IsNullOrEmpty(nameFile))
+            {
+                return;
+            }
+            else
+            {
+                string file = Path.Combine(System.IO.Directory.GetCurrentDirectory(), @"FileTests\", nameFile);
+                if (File.Exists(file))
+                {
+                    using (var model = IfcStore.Open(file))
+                    {
+                        var context = new Xbim3DModelContext(model);
+                        context.CreateContext();
+
+                        string fileWexbim = Path.ChangeExtension(file, "wexBIM");
+
+                        using (var fileCreate = File.Create(fileWexbim))
+                        {
+                            using (var fileWriter = new BinaryWriter(fileCreate))
+                            {
+                                model.SaveAsWexBim(fileWriter);
+                                fileWriter.Close();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Arquivo não encontrado");
+                }
+            }
+            Execute();
+        }
+    }
+}
